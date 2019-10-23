8db334a63ab7d40c42ce4df32819bcb8f5cd1165
fanzhangg <vanadiumzhang@gmail.com>
2019-10-23 11:12:42 -0500

    ADD: a l-system parser

---
+++ examples/l-system_parser/src/LSystem.js
@@ -0,0 +1,101 @@
+class Lsystem{
+    constructor(){
+        this.axiom = "";
+        this.rules = {};
+        this.iterations = 0;
+        this.onclick();
+    }
+
+    generate(){
+        const sentence = this.getSentence(this.iterations);
+        const result = document.getElementById("result");
+        result.innerText = sentence;
+    }
+
+    getData(e){
+        const form = document.getElementById("parameters");
+        const formData = new FormData(form);
+        if (formData.get("axiom").match(/^[A-Z]$/)==null){
+            alert("Invalid axiom input. Axiom must be a capitalized letter!");
+            return false;
+        }
+        this.axiom = formData.get("axiom");
+
+        if (formData.get("iterations").match(/^[0-9]+$/)==null){
+            alert("Invalid iterations input. Iterations must be a positive number!");
+            return false;
+        }
+        this.iterations = formData.get("iterations");
+
+        if (formData.get("rule1") === ""){  // Assume people enter the rule sequentially
+            return true;
+        }
+        if (formData.get("rule1").match(/^[A-Z]=[A-Z+\-\[\]]+/) == null){
+            alert("Invalid rule1 input. Rule must be A=XYZ");
+            return false;
+        }
+
+        let start;
+        let end;
+        [start, end] = formData.get("rule1").split("=");
+        this.rules[start] = end;
+
+        if (formData.get("rule2") === ""){  // Assume people enter the rule sequentially
+            return true;
+        }
+        if (formData.get("rule2").match(/^[A-Z]=[A-Z+\-\[\]]+/) == null) {
+            alert("Invalid rule2 input. Rule must be A=XYZ");
+            return false;
+        }
+        [start, end] = formData.get("rule2").split("=");
+        this.rules[start] = end;
+
+        if (formData.get("rule3") === ""){  // Assume people enter the rule sequentially
+            return true;
+        }
+        if (formData.get("rule3").match(/^[A-Z]=[A-Z+\-\[\]]+/) == null) {
+            alert("Invalid rule3 input. Rule must be A=XYZ");
+            return false;
+        }
+        [start, end] = formData.get("rule3").split("=");
+        this.rules[start] = end;
+        return true;
+    }
+
+    onclick(){
+        const button = document.getElementById("generateButton");
+        button.addEventListener('pointerup', (e) => {
+            const isValidData = this.getData(e);
+            if (isValidData){
+                this.generate();
+            }
+        });
+        // const formData = $('form').serializeArray();
+        // const axiom = formData["axiom"];
+        // console.log(formData);
+    }
+
+    getNextSentence(currSentence){
+        let nextSentence = "";
+        for (let char of currSentence){ // Loop through each char in the currSentence
+            let rule = this.rules[char];
+            if (typeof rule === "undefined"){   // The char does not have a rule
+                nextSentence += char;   // Do not change the char
+            } else {
+                nextSentence += rule;   // Replace the char with the derivation
+            }
+        }
+        console.log(`Next sentence: "${nextSentence}"`);
+        return nextSentence;
+    }
+
+    getSentence(iters){
+        let sentence = this.axiom;
+        for (let i = 0; i < iters; i++){
+            sentence = this.getNextSentence(sentence);
+        }
+        return sentence;
+    }
+}
+
+const ls = new Lsystem();
